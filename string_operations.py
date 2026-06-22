f48ab125265ac7413448dd212257ee6a1de26f1b
aaliyaakhtar07 <aaliyaakhtar07@gmail.com>
2026-06-22 18:55:50 +0530

    string operations

---
+++ string_operations.py
@@ -0,0 +1,10 @@
+s1="Hello"
+s2="World"
+s3=s1+" "+s2  #String concatenation
+s4=s1*3  #String repetition
+s5=s1[0]  #Accessing first character
+s6=s1[1:4]  #Slicing the string
+print('H' in s1) #membership operaator
+print("Concatenated String:", s3)
+print("Repeated String:", s4)
+print("First Character:", s5)
