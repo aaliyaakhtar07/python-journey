2b0671993c17e2fa395e9e3fb09923c5a7d6fd25
aaliyaakhtar07 <aaliyaakhtar07@gmail.com>
2026-06-24 20:41:41 +0530

    Arithmetic and comparison operators

---
+++ arithmetic_operators.py
@@ -0,0 +1,16 @@
+x=10
+y=20
+print("Addition:", x+y)  #Output: 30
+print("Subtraction:", x-y)  #Output: -10
+print("Multiplication:", x*y)  #Output: 200
+print("Division:", x/y)  #Output: 0.5
+print("Floor Division:", x//y)  #Output: 0
+print("Modulus:", x%y)  #Output: 10
+print("Exponentiation:", x**y)  #Output: 102400000000000000000
+print("Comparison Operators:")
+print("Equal:", x==y)  #Output: False
+print("Not Equal:", x!=y)  #Output: True
+print("Greater Than:", x>y)  #Output: False 
+print("Less Than:", x<y)  #Output: True
+print("Greater Than or Equal To:", x>=y)  #Output: False
+print("Less Than or Equal To:", x<=y)  #Output: True
