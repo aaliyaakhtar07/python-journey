1228712d9b2ac9ce5c462312d47bb78543e1183a
aaliyaakhtar07 <aaliyaakhtar07@gmail.com>
2026-06-27 22:37:03 +0530

    Assignment Operators

---
+++ assignment_operators.py
@@ -0,0 +1,29 @@
+a=10
+b=20
+#Assignment operators are used to assign values to variables.
+c=a+b  # c is assigned the value of a + b
+print("Value of c:", c)  # Output: Value of c: 30
+a+=5  # a is incremented by 5, equivalent to a = a + 5
+print("Value of a:", a)  # Output: Value of a: 15
+a-=3  # a is decremented by 3, equivalent to a = a - 3
+print("Value of a:", a)  # Output: Value of a: 12
+b*=2  # b is multiplied by 2, equivalent to b = b *2
+print("Value of b:", b)  # Output: Value of b: 40
+b/=4  # b is divided by 4, equivalent to b = b / 4
+print("Value of b:", b)  # Output: Value of b: 10.0     
+b%=3  # b is assigned the remainder of b divided by 3, equivalent to b = b % 3
+print("Value of b:", b)  # Output: Value of b: 1.0
+b**=2  # b is raised to the power of 2, equivalent to b = b ** 2
+print("Value of b:", b)  # Output: Value of b: 1.0
+b//=2  # b is floor divided by 2, equivalent to b = b // 2
+print("Value of b:", b)  # Output: Value of b: 0.0
+a&=2  # a is bitwise ANDed with 2, equivalent to a = a & 2
+print("Value of a:", a)  # Output: Value of a: 0
+a|=3  # a is bitwise ORed with 3, equivalent to a = a | 3
+print("Value of a:", a)  # Output: Value of a: 3
+a^=1  # a is bitwise XORed with 1, equivalent to a = a ^ 1
+print("Value of a:", a)  # Output: Value of a: 2
+a<<=1  # a is left shifted by 1, equivalent to a = a << 1
+print("Value of a:", a)  # Output: Value of a: 4
+a>>=1  # a is right shifted by 1, equivalent to a = a >> 1
+print("Value of a:", a)  # Output: Value of a: 2
