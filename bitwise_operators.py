b84fcac2d371fccedc888507ecf6e6b594a014fd
aaliyaakhtar07 <aaliyaakhtar07@gmail.com>
2026-06-28 22:38:18 +0530

    Bitwise Operators

---
+++ bitwise_operators.py
@@ -0,0 +1,9 @@
+a=10
+b=5
+#Bitwise operators are used to compare (binary) numbers. The six bitwise operators in Python are &, |, ^, ~, <<, and >>.
+print(a & b)  # Output: 0
+print(a | b)  # Output: 15
+print(a ^ b)  # Output: 15
+print(~a)     # Output: -11
+print(a << 2) # Output: 40
+print(a >> 2) # Output: 2
