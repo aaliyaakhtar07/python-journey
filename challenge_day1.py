0150374edf1a737bdd4451fe7399fedbde83f912
aaliyaakhtar07 <aaliyaakhtar07@gmail.com>
2026-06-29 22:42:46 +0530

    Practice

---
+++ challenge_day1.py
@@ -0,0 +1,13 @@
+#Write a code that initializes two variables, x and y, with the values 12.5 and 3.5 (respectively).
+#After that, initialize another variable z that will hold the result of x * y.
+x = 12.5
+y = 3.5
+z = x * y
+print(f"x = {x}, y = {y}, z = {z}")
+
+#Write a code that initializes two variables, a and b, with the values 5.2 and 2.6 (respectively).
+#After that, initialize another variable c that will hold the result of a / b.
+a = 5.2
+b = 2.6
+c = a / b
+print(f"a = {a}, b = {b}, c = {c}")
