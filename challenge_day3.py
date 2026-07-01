bf1f31d74e739a6a7c3136aa001cb9324bf8175d
aaliyaakhtar07 <aaliyaakhtar07@gmail.com>
2026-07-01 22:05:52 +0530

    Arithmrtic Shortcuts

---
+++ challenge_day3.py
@@ -0,0 +1,25 @@
+'''You are given a code with initialization of count. 
+Your task is to add the following operations, in this order:
+Add 4 to count
+Multiply count by 2
+Subtract 1 from count
+Use the arithmetic shortcuts to do so!'''
+
+count = 0
+count += 4
+count *= 2 
+count -= 1
+print(count)
+
+'''You are given a code with initialization of score. 
+Your task is to modify the score variable using arithmetic shortcuts, in this order:
+Divide score by 2
+Add 10 to score
+Multiply score by 3
+Remember to use the arithmetic shortcuts for each operation!'''
+
+score = 100
+score /= 2
+score += 10
+score *= 3
+print(score)
