ceaccb90c1fd2f22f3aa69f480e7c1ed667e3533
aaliyaakhtar07 <aaliyaakhtar07@gmail.com>
2026-07-11 22:02:09 +0530

    Left Pyramid pattern

---
+++ left_pyramid_of_stars.py
@@ -0,0 +1,14 @@
+#Program to print left pyramid of stars
+rows=int(input("Enter number of rows: "))
+for i in range(1, rows + 1):
+    for space in range(1, rows - i + 1):
+        print(" ", end="")
+    for star in range(1, i + 1):
+        print("*", end="")
+    print()
+for j in range(rows -1, 0, -1):
+    for space in range(1, rows - j + 1):
+        print(" ", end="")
+    for star in range(1, j + 1):
+        print("*", end="")
+    print()
